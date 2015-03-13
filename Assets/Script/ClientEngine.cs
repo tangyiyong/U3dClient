abd102cdab3e2dc2404d8814b547abb0e2a36f68
ylmbtm@163.com <ylmbtm@163.com>
2015-03-13 15:47:40 +0800

    Upload Code

---
+++ Assets/Script/ClientEngine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System; 
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ClientEngine: MonoBehaviour {
+
+    public static ClientConnector s_ClientConnector = null;
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        s_ClientConnector = new ClientConnector();
+        s_ClientConnector.InitConnector();
+    }
+
+    void OnDestroy()
+    {
+        if (s_ClientConnector != null)
+        {
+            s_ClientConnector.CloseConnector();
+        }
+    }
+
+    void Update()
+    {
+        if (s_ClientConnector != null)
+        {
+            s_ClientConnector.Render();
+        }
+    }
+}
--- Assets/Script/LoginUI.cs
+++ Assets/Script/LoginUI.cs
@@ -4,15 +4,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class LoginUI : MonoBehaviour {
-
+public class LoginUI : MonoBehaviour
+{
     public string ui_state = "login";
  
-    private string labelMsg = "";
-    private Color labelColor = Color.green;
+    private string m_strLabelMsg  = "";
+    private Color m_LabelColor    = Color.green;
 
-    private string stringAccount = "";
-    private string stringPasswd = "";
+    private string m_stringAccount = "";
+    private string m_stringPasswd  = "";
+
+    private StCharPickInfo[] m_SelCharList = new StCharPickInfo[4];
 
     void Start()
     {
@@ -29,99 +31,40 @@ public class LoginUI : MonoBehaviour {
     {
 
     }
-    /*
-    void onSelAvatarUI()
+    
+    void ShowSelectUI()
     {
-        if (startCreateAvatar == false && GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 40, 200, 30), "CreateAvatar(创建角色)"))    
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 40, 200, 30), "CreateAvatar(创建角色)"))    
         {
-            startCreateAvatar = !startCreateAvatar;
         }
 
-        if (startCreateAvatar == false && GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 75, 200, 30), "EnterGame(进入游戏)"))    
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 75, 200, 30), "EnterGame(进入游戏)"))    
         {
-            if(selAvatarDBID == 0)
-            {
-                err("Please select a Avatar!(请选择角色!)");
-            }
-            else
-            {
-                info("Please wait...(请稍后...)");
-                Account account = (Account)KBEngineApp.app.player();
-                if(account != null)
-                    account.selectAvatarGame(selAvatarDBID);
-				
-                Application.LoadLevel("world");
-            }
         }
 		
-        if(startCreateAvatar)
+        for(int i = 0; i < 4; i++)
         {
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 40, 200, 30), "CreateAvatar-OK(创建完成)"))    
-            {
-                if(stringAvatarName.Length > 1)
-                {
-                    startCreateAvatar = !startCreateAvatar;
-                    Account account = (Account)KBEngineApp.app.player();
-                    account.reqCreateAvatar(1, stringAvatarName);
-                }
-                else
-                {
-                    err("avatar name is null(角色名称为空)!");
-                }
-            }
-	        
-            stringAvatarName = GUI.TextField(new Rect(Screen.width / 2 - 100, Screen.height - 75, 200, 30), stringAvatarName, 20);
+            GUI.Label(Rect(0,i * 20,100,20),"名字");
+            GUI.Label(Rect(100,i * 20,100,20),"等级");
+            GUI.Label(Rect(200,i * 20,100,20),"职业");
+            GUI.Label(Rect(300,i * 20,100,20),"性别");
         }
-		
-        if(ui_avatarList != null && ui_avatarList.Count > 0)
-        {
-            int idx = 0;
-            foreach(UInt64 dbid in ui_avatarList.Keys)
-            {
-                Dictionary<string, object> info = ui_avatarList[dbid];
-                Byte roleType = (Byte)info["roleType"];
-                string name = (string)info["name"];
-                UInt16 level = (UInt16)info["level"];
-                UInt64 idbid = (UInt64)info["dbid"];
 
-                idx++;
-				
-                Color color = GUI.contentColor;
-                if(selAvatarDBID == idbid)
-                {
-                    GUI.contentColor = Color.red;
-                }
-				
-                if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 120 - 35 * idx, 200, 30), name))    
-                {
-                    Debug.Log("selAvatar:" + name);
-                    selAvatarDBID = idbid;
-                }
-				
-                GUI.contentColor = color;
-            }
-        }
-        else
-        {
-            if(KBEngineApp.app.entity_type == "Account")
-            {
-                KBEngine.Account account = (KBEngine.Account)KBEngineApp.app.player();
-                if(account != null)
-                    ui_avatarList = new Dictionary<ulong, Dictionary<string, object>>(account.avatars);
-            }
-        }
+        return ;
     }
-    */
+    
     void ShowLoginUI()
     {
-        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 30, 200, 30), "Login(登陆)"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 30, 200, 30), "登陆"))
         {
             Debug.Log("stringAccount:" + stringAccount);
             Debug.Log("stringPasswd:" + stringPasswd);
 
             if (stringAccount.Length > 0 && stringPasswd.Length > 5)
             {
-
+                ClientEngine.s_ClientConnector.m_strLoginIp = "127.0.0.1";
+                ClientEngine.s_ClientConnector.m_sLoginPort = 7994;
+                ClientEngine.s_ClientConnector.Login(stringAccount, stringPasswd);
             }
             else
             {
@@ -129,7 +72,7 @@ public class LoginUI : MonoBehaviour {
             }
         }
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 70, 200, 30), "CreateAccount(注册账号)"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 70, 200, 30), "注册账号"))
         {
             Debug.Log("stringAccount:" + stringAccount);
             Debug.Log("stringPasswd:" + stringPasswd);
@@ -145,7 +88,7 @@ public class LoginUI : MonoBehaviour {
         }
 
         stringAccount = GUI.TextField(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 30), stringAccount, 20);
-        stringPasswd = GUI.PasswordField(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 10, 200, 30), stringPasswd, '*');
+        stringPasswd  = GUI.PasswordField(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 10, 200, 30), stringPasswd, '*');
     }
 
     void OnGUI()
@@ -154,8 +97,90 @@ public class LoginUI : MonoBehaviour {
         {
             ShowLoginUI();
         }
+        else if (ui_state == "select")
+        {
+            ShowSelectUI();
+        }
 
         GUI.contentColor = labelColor;
         GUI.Label(new Rect((Screen.width / 2) - 100, 40, 400, 100), labelMsg);
-    }  
+    }
+
+    void OnCharLoginAck(ReadBufferHelper ReadHelper)
+    {
+        StCharLoginAck Ack = new StCharLoginAck();
+        Ack.Read(ReadHelper);
+
+        if (Ack.nRetCode != 0)
+        {
+            labelMsg = "登录失败!!!";
+            return;
+        }
+
+        for (int i = 0; i < Ack.Count; i++)
+        {
+            m_SelCharList[i] = new StCharPickInfo();
+             m_SelCharList[i] = Ack.CharPickInfo[i];
+        }
+
+        ui_state = "select";
+
+        return;
+    }
+
+
+    public override Boolean OnCommandHandle(Command_ID wCommandID, UInt64 u64ConnID, ReadBufferHelper ReadHelper)
+    {
+        switch (wCommandID)
+        {
+            case Command_ID.CMD_CHAR_LOGIN_ACK:
+                {
+                    OnCharLoginAck(ReadHelper);
+                }
+                break;
+            case Command_ID.CMD_CHAR_NEW_CHAR_ACK:
+                {
+
+                }
+                break;
+            case Command_ID.CMD_CHAR_DEL_CHAR_ACK:
+                {
+
+                }
+                break;
+            case Command_ID.CMD_CHAR_NEW_ACCOUNT_ACK:
+                {
+                    UInt16 nRetCode = ReadHelper.ReadUint16();
+                }
+                break;
+            case Command_ID.CMD_CHAR_ENTER_GAME_ACK:
+                {
+
+                }
+                break;
+            case Command_ID.CMD_CHAR_NEARBY_ADD:
+                {
+
+                }
+                break;
+            case Command_ID.CMD_CHAR_NEARBY_UPDATE:
+                {
+
+                }
+                break;
+            case Command_ID.CMD_CHAR_NEARBY_REMOVE:
+                {
+
+                }
+                break;
+
+            default:
+                {
+
+                }
+                break;
+        }
+
+        return true;
+    }
 }
